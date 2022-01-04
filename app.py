36716d7dabb8938f2e4ac003eb7fa53ffab80381
MasterMeep <51376656+MasterMeep@users.noreply.github.com>
2022-01-04 16:25:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,101 +1,62 @@
 # This file is based on: https://raw.githubusercontent.com/streamlit/demo-uber-nyc-pickups/master/app.py
-
-"""An example of showing geographic data."""
-
 import streamlit as st
-import pandas as pd
-import numpy as np
-import altair as alt
-import pydeck as pdk
+from datetime import date
 
-DATE_TIME = "date/time"
-DATA_URL = (
-    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
-)
-TOTAL_SAMPLES = 100000
+import yfinance as yf
+from plotly import graph_objs as go
 
-st.title("Uber Pickups in New York City")
-st.markdown(
-    """
-This is a demo of a Streamlit app that shows the Uber pickups
-geographical distribution in New York City. Use the slider
-to pick a specific hour and look at how the charts change.
+START = "2018-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
 
-[See source code](https://github.com/streamlit/demo-uber-nyc-pickups/blob/master/app.py)
-"""
-)
+st.title("Stonks")
+
+stocks = ("GOOGL", "TSLA", "AMZN", "FB")
+selected_stock = st.selectbox("Select stock for prediction", stocks)
+current_period = st.select_slider(
+     'Select a color of the rainbow',
+     options=['5d',"1mo","3mo","6mo","1y",'2y','5y','10y','ytd','max'])
 
 
-@st.cache(persist=True)
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    data.rename(lambda x: str(x).lower(), axis="columns", inplace=True)
-    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, period = current_period)
+    data.reset_index(inplace=True)
     return data
 
+load_state = st.text("Loading Data...")
+data = load_data(selected_stock)
+load_state.text("Loading Data... Done!")
 
-def run():
-    data = load_data(TOTAL_SAMPLES)
+load_state.text("")
 
-    hour = st.slider("Hour to look at", 0, 23)
+def plot_data():
+    fig = go.Figure()
 
-    data = data[data[DATE_TIME].dt.hour == hour]
+    fig.add_trace(go.Scatter(x=data["Date"], y=data["Close"], name="Close"))
+    fig.add_trace(go.Scatter(x=data["Date"], y=data["Open"], name="Open"))
 
-    st.subheader("Geo data between %i:00 and %i:00" % (hour, (hour + 1) % 24))
-    midpoint = (np.average(data["lat"]), np.average(data["lon"]))
+    fig.layout.update(xaxis_rangeslider_visible = True)
+    st.plotly_chart(fig)
 
-    st.write(
-        pdk.Deck(
-            map_style="mapbox://styles/mapbox/light-v9",
-            initial_view_state={
-                "latitude": midpoint[0],
-                "longitude": midpoint[1],
-                "zoom": 11,
-                "pitch": 50,
-            },
-            layers=[
-                pdk.Layer(
-                    "HexagonLayer",
-                    data=data,
-                    get_position=["lon", "lat"],
-                    radius=100,
-                    elevation_scale=4,
-                    elevation_range=[0, 1000],
-                    pickable=True,
-                    extruded=True,
-                ),
-            ],
-        )
-    )
+st.title("Open/Close Data")
+plot_data()
 
-    st.subheader(
-        "Breakdown by minute between %i:00 and %i:00" % (hour, (hour + 1) % 24)
-    )
-    filtered = data[
-        (data[DATE_TIME].dt.hour >= hour) & (data[DATE_TIME].dt.hour < (hour + 1))
-    ]
-    hist = np.histogram(filtered[DATE_TIME].dt.minute, bins=60, range=(0, 60))[0]
-    chart_data = pd.DataFrame({"minute": range(60), "pickups": hist})
-
-    st.altair_chart(
-        alt.Chart(chart_data)
-        .mark_area(
-            interpolate="step-after",
-        )
-        .encode(
-            x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
-            y=alt.Y("pickups:Q"),
-            tooltip=["minute", "pickups"],
-        ),
-        use_container_width=True,
-    )
-
-    if st.checkbox("Show raw data", False):
-        st.subheader(
-            "Raw data by minute between %i:00 and %i:00" % (hour, (hour + 1) % 24)
-        )
-        st.write(data)
+st.caption("Raw Data")
+st.write(data)
 
 
-if __name__ == "__main__":
-    run()
+
+st.title("Info")
+info_state = st.text("Loading Data...")
+
+stock_information = yf.Ticker(selected_stock).info
+
+st.markdown(f"**Industry**: {stock_information['industry']}")
+st.markdown(f"**Current Price**: ${'{:,}'.format(stock_information['currentPrice'])}")
+st.markdown(f"**Yahoo Finance Recommendation**: {stock_information['recommendationKey']}")
+st.markdown(f"**Gross Profits**: {'{:,}'.format(stock_information['grossProfits'])}")
+st.markdown(f"**Enterprise Value**: {'{:,}'.format(stock_information['enterpriseValue'])}")
+
+
+info_state.text("Loading Data... Done!")
+info_state.text("")
